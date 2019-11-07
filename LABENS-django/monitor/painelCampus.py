f03e4d4fbabcdcb98ead0ff804de60d2a39e8d64
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-11-07 17:30:56 -0300

    Pegando dados ambientais das tabelas novas

--- LABENS-django/monitor/painelCampus.py
+++ LABENS-django/monitor/painelCampus.py
@@ -65,8 +65,8 @@ def painel(request,campus):
     StationTypes = ['SONDA','EPE']
     StationType = StationTypes[campus.estTipo]
 
-    ambFile = paths.Ftp()+campus.cod.upper()+'_'+StationType+'/TAB_SCADA.DAT'
-    pluvFile = paths.Ftp()+campus.cod.upper()+'_'+StationType+'/TAB_RAD_10.DAT'
+    radFile = paths.Ftp()+campus.cod.upper()+'_'+StationType+'/TAB_RAD_01.DAT'
+    metFile = paths.Ftp()+campus.cod.upper()+'_'+StationType+'/TAB_RAD_10.DAT'
 
     #Leva a data para a meia noite do dia atual para comparar com o tempo dos arquivos do ftp
     initialTime = datetime.datetime.strptime(data.strftime('%Y%m%d'),'%Y%m%d')
@@ -95,9 +95,9 @@ def painel(request,campus):
         irradiancia.append({'titulo':'Direta Normal','valor':'N/D'})
         irradiancia.append({'titulo':'Difusa','valor':'N/D'})
 
-    if os.path.isfile(ambFile):
-        datFile = open(ambFile, newline='')
-        reader = csv.reader(datFile, delimiter=',')
+    if os.path.isfile(radFile):
+        datRad = open(radFile, newline='')
+        reader = csv.reader(datRad, delimiter=',')
         next(reader)
         next(reader)
         next(reader)
@@ -105,46 +105,38 @@ def painel(request,campus):
         for row in reader:
             entrydate = datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S')
             if entrydate >= initialTime and entrydate <= finalTime:
-                if campus.estTipo == 0:
-                    irradianciaGraf['Global'].append(row[7])
-                    irradianciaGraf['Inclinado'].append(row[9])
+                    irradianciaGraf['Global'].append(row[6])
+                    irradianciaGraf['Inclinado'].append(row[32])
 
-                    dadosMeterologicos[0]['valor'] = float(row[14]) #T Ambiente
-                    dadosMeterologicos[1]['valor'] = float(row[15]) #Umidade
-                    dadosMeterologicos[2]['valor'] = float(row[20]) #V Vento
-                    dadosMeterologicos[3]['valor'] = float(row[21]) #Dir Vento
-                    dadosMeterologicos[4]['valor'] = float(row[13]) #Pressão
+                    irradiancia[0]['valor'] = float(row[32]) #Plano Inclinado
+                    irradiancia[1]['valor'] = float(row[6]) #Global Horizontal
 
-                    irradiancia[0]['valor'] = float(row[9]) #Plano Inclinado
-                    irradiancia[1]['valor'] = float(row[7]) #Global Horizontal
-                    irradiancia[2]['valor'] = float(row[10]) #Direta Normal
-                    irradiancia[3]['valor'] = float(row[8]) #Difusa
+                    if campus.estTipo == 0:
+                        irradiancia[2]['valor'] = float(row[10]) #Direta Normal
+                        irradiancia[3]['valor'] = float(row[14]) #Difusa
 
-                else:
-                    irradianciaGraf['Global'].append(row[4])
-                    irradianciaGraf['Inclinado'].append(row[5])
+        datRad.close()
 
-                    dadosMeterologicos[0]['valor'] = float(row[6]) #T Ambiente
-                    dadosMeterologicos[1]['valor'] = float(row[7]) #Umidade
-                    dadosMeterologicos[2]['valor'] = float(row[8]) #V Vento
 
-                    irradiancia[0]['valor'] = float(row[5]) #Plano Inclinado
-                    irradiancia[1]['valor'] = float(row[4]) #Global Horizontal
 
-        datFile.close()
+    if os.path.isfile(metFile):
+        datMet = open(metFile, newline='')
+        reader = csv.reader(datMet, delimiter=',')
+        next(reader)
+        next(reader)
+        next(reader)
+        next(reader)
+        for row in reader:
+            dadosMeterologicos[0]['valor'] = float(row[14]) #T Ambiente
+            dadosMeterologicos[1]['valor'] = float(row[15]) #Umidade
+            dadosMeterologicos[2]['valor'] = float(row[6]) #V Vento
 
-        if os.path.isfile(pluvFile) and campus.estTipo == 0:
-            datPluv = open(pluvFile, newline='')
-            readerPluv = csv.reader(datPluv, delimiter=',')
+            if campus.estTipo == 0:
+                dadosMeterologicos[3]['valor'] = float(row[10]) #Dir Vento
+                dadosMeterologicos[4]['valor'] = float(row[16]) #Pressão
+                dadosMeterologicos[5]['valor'] = float(row[17]) #Pluviosidade
 
-            next(readerPluv)
-            next(readerPluv)
-            next(readerPluv)
-            next(readerPluv)
-            for row in readerPluv:
-                dadosMeterologicos[5]['valor'] = float(row[17])
-
-            datPluv.close()
+        datMet.close()
 
     context = {'campus':campus,
                 'estTipo': StationType,
