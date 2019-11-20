985abc97453ce1fd3b1089722132a10fa7095955
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-11-20 16:36:53 -0300

    Adicionando pedido de senha

--- LABENS-django/monitor/views.py
+++ LABENS-django/monitor/views.py
@@ -1,18 +1,63 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.utils.http import urlencode
 from . import painelCampus
 from .models import Campus
+from django.views.decorators.csrf import csrf_exempt
+import datetime
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    sessao = request.session.get('status', 0)
+
+    if sessao:
+        return render(request,'index.html')
+    else:
+        return redirect('/login?'+urlencode({'redirect':'/'}))
+
+@csrf_exempt
+def login(request):
+    if request.method == 'GET':
+        redir = ''
+        if 'redirect' in request.GET:
+            redir = request.GET['redirect']
+
+        return render(request,'login.html',{'redirect':redir,'loginErr':0})
+    else:
+        if not 'redirect' in request.POST or not 'passwd' in request.POST:
+            return redirect('/login')
+
+        if request.POST['passwd'] == 'Senha Aqui':
+                request.session['status'] = 1
+        else:
+            return render(request,'login.html',{'redirect':request.POST['redirect'],'loginErr':1})
+
+        if not request.POST['redirect'] == '':
+            return redirect(request.POST['redirect'])
+        else:
+            return redirect('/')
 
 def selectCampus(request):
-    campi = Campus.objects.all()
-    return render(request,'painelSelecCampus.html',{'campi':campi})
+    sessao = request.session.get('status', 0)
+
+    if sessao:
+        campi = Campus.objects.all()
+        return render(request,'painelSelecCampus.html',{'campi':campi})
+    else:
+        return redirect('/login?'+urlencode({'redirect':'/painelCampus'}))
 
 def showPainelCampus(request,campus):
-    return painelCampus.painel(request,campus)
+    sessao = request.session.get('status', 0)
+
+    if sessao:
+        return painelCampus.painel(request,campus)
+    else:
+        return redirect('/login?'+urlencode({'redirect':'/painelCampus/'+campus}))
 
 def indicesDeMerito(request):
-    return render(request,'indicesDeMerito.html')
+    sessao = request.session.get('status', 0)
+
+    if sessao:
+        return render(request,'indicesDeMerito.html')
+    else:
+        return redirect('/login?'+urlencode({'redirect':'/indicesDeMerito'}))
