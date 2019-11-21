b2c21876c37517fa7185276ca6b88acd7369af1d
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2019-11-21 14:47:22 -0300

    Corrigindo erro na key que pega o IP.

--- LABENS-django/monitor/views.py
+++ LABENS-django/monitor/views.py
@@ -23,7 +23,7 @@ def login(request):
         if 'redirect' in request.GET:
             redir = request.GET['redirect']
 
-        if ipaddress.IPv4Address(request.META['HTTP_X_FORWARDED_FOR'])>ipaddress.IPv4Address('200.134.0.0') and ipaddress.IPv4Address(request.META['HTTP_X_FORWARDED_FOR'])<ipaddress.IPv4Address('200.134.127.255'):
+        if ipaddress.IPv4Address(request.META['REMOTE_ADDR'])>ipaddress.IPv4Address('200.134.0.0') and ipaddress.IPv4Address(request.META['REMOTE_ADDR'])<ipaddress.IPv4Address('200.134.127.255'):
             request.session['status'] = 1
             if not redir == '':
                 return redirect(redir)
