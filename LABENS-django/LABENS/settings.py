1e9b7819861ce002589f24591183516fbd7958d3
João Pedro Galera <joaosilva.2016@alunos.utfpr.edu.br>
2020-12-13 16:21:35 -0300

    Correção

--- LABENS-django/LABENS/settings.py
+++ LABENS-django/LABENS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-af&nkgqwle#7p0xf-=as&r6f&zfw0b@^!o+)!wvmx08m818vy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
